b835bbb8f2b3d4287aaf97e3dad399c96178fc57
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2022-01-21 17:15:15 -0300

    Payment, one-to-one association

--- src/main/java/com/antunes/webservices/config/TestConfig.java
+++ src/main/java/com/antunes/webservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.antunes.webservices.entities.Category;
 import com.antunes.webservices.entities.Order;
 import com.antunes.webservices.entities.OrderItem;
+import com.antunes.webservices.entities.Payment;
 import com.antunes.webservices.entities.Product;
 import com.antunes.webservices.entities.User;
 import com.antunes.webservices.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepo.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
